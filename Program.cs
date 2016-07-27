a3f6a9b654247e8dabc903b1c634753809c9ff26
Fredrik Eriksson <fredrik@eriksson.cm>
2016-07-27 11:48:02 +0200

    Move test.json to input.json.
    The code in Program.cs is the same as the example in README.md.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,39 @@
 ﻿using System;
 using System.IO;
-using System.Text;
-using System.Collections.Generic;
-using System.Diagnostics;
 
 using Json;
 
-namespace ConsoleApplication {
-    public class Program {
-        public static void Main(string[] args) {
-            var sw = new Stopwatch();
-            sw.Start();
-            Result<JObject> o;
-            using (var s = File.OpenRead("citylots.json")) {
-                o = JObject.Parse(s);
+static class Program {
+    static void Main() {
+        var arr = new JArray {
+            new JObject {
+                ["id"] = "a",
+                ["str"] = null,
+                ["date"] = DateTime.UtcNow,
+                ["int"] = int.MaxValue,
+                ["double"] = double.MaxValue, 
+                ["obj"] = new JObject {
+                    ["id"] = "b"
+                }
             }
+        };
+
+        Console.WriteLine(arr);
+        Console.WriteLine(arr.ToString(ws: false));
+
+        using (var f = File.OpenWrite("output.json")) {
+            arr.Write(f);
+        }
+
+        var o = JObject.Parse("{ \"result\" : 42 }");
+        if (o.Failed) {
+            // ERROR
+        }
+        using (var f = File.OpenRead("input.json")) {
+            o = JObject.Parse(f);
             if (o.Failed) {
-                Console.WriteLine($"ERROR: {o.ErrorCode}");
-                return;
+                // ERROR
             }
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed);
         }
     }
 }
